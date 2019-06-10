5b4edbe0ef551ba787ef3a59b17dc495bdfd9681
Guru test <aws9guru@gmail.com>
2019-06-10 21:20:13 +0530

    Updated DBjava file

---
+++ DBConnect.java
@@ -0,0 +1,10 @@
+package com.h2.examples;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.h2.tools.DeleteDbFiles;
